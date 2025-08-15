cdfdda7aeccb8c63e6e25bc87e2f28884d78182d
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-08-15 15:21:25 -0400

    Ray class done

--- src/core/Ray.h
+++ src/core/Ray.h
@@ -1,3 +1,14 @@
 #pragma once
 
-// Ray class with origin and direction
+class Ray {
+    public:
+        Vec3 origin;
+        Vec3 direction;
+
+        Ray(const Vec3& origin, const Vec3& direction)
+            : origin(origin), direction(direction.Normalize()) {}
+
+        Vec3 At(float t) const {
+            return origin + direction * t;
+        }
+};
