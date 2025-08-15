4eee9df63a73d262f936b90295a54912b4ddada3
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-08-15 16:02:20 -0400

    scene class

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -1,1 +1,25 @@
-// Scene implementation
+#include "Scene.h"
+
+void Scene::Add(std::shared_ptr<Hittable> object) {
+    objects.push_back(object);
+}
+
+void Scene::Clear() {
+    objects.clear();
+}
+
+bool Scene::Hit(const Ray& ray, float tMin, float tMax, HitRecord& record) const {
+    HitRecord tempRecord;
+    bool hitAnything = false;
+    float closestSoFar = tMax;
+
+    for (const auto& object : objects) {
+        if (object->Hit(ray, tMin, closestSoFar, tempRecord)) {
+            hitAnything = true;
+            closestSoFar = tempRecord.t;
+            record = tempRecord;
+        }
+    }
+
+    return hitAnything;
+}
--- src/scene/Scene.h
+++ src/scene/Scene.h
@@ -1,3 +1,21 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include "Hittable.h"
 
-// Scene containing objects and materials
+class Scene : public Hittable {
+private:
+    std::vector<std::shared_ptr<Hittable>> objects;
+
+public:
+    // Constructors
+    Scene() = default;
+    ~Scene() = default;
+
+    // Object management
+    void Add(std::shared_ptr<Hittable> object);
+    void Clear();
+    
+    // Ray intersection (inherited from Hittable)
+    virtual bool Hit(const Ray& ray, float tMin, float tMax, HitRecord& record) const override;
+};
