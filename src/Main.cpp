50cd27a0ce1cebf40bf2eddf40baf44f91ea9925
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-08-18 19:45:45 -0400

    cube added to scene

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include "scene/Scene.h"
 #include "geometry/Sphere.h"
 #include "geometry/Plane.h"
+#include "geometry/Box.h"
 #include "scene/Camera.h"
 #include "renderer/Renderer.h"
 #include "utils/Image.h"
@@ -32,7 +33,9 @@ int main() {
     // Add a main sphere
     auto centerMaterial = std::make_shared<Lambertian>(Vec3(0.7f, 0.3f, 0.3f));
     scene.Add(std::make_shared<Sphere>(Vec3(0, 0, -1), 0.5f, centerMaterial));
-    
+
+    scene.Add(std::make_shared<Box>(Vec3(1, 0, -1), Vec3(0.2f, 0.2f, 0.2f), centerMaterial));
+
     // Create camera
     Camera camera;
     
